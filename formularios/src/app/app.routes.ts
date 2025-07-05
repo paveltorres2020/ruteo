50c68dce05a8f4e2703338b5e57f6f025d077b18
Pavel Torres <paveltorre2020@gmail.com>
2025-07-05 11:54:49 -0500

    final

--- formularios/src/app/app.routes.ts
+++ formularios/src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 import { Normal } from './normal/normal';
 import { Reactivo } from './reactivo/reactivo';
 
 export const routes: Routes = [
-  { path: 'home', component: Normal },
-  { path: 'about', component: Reactivo },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: 'Normal', pathMatch: 'full' },
+  { path: 'Normal', component: Normal },
+  { path: 'Reactivo', component: Reactivo }
+  
 ];
--- formularios/src/app/app.ts
+++ formularios/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'formularios';
 }
+
+
